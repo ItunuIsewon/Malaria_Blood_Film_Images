8b158942b346f167ad676e57608081e537452ec5
ItunuIsewon <73642453+ItunuIsewon@users.noreply.github.com>
2023-11-14 22:01:25 +0100

    Delete markmap-lib directory

--- markmap-lib/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './types';
-export * from './constants';
-export * from './transform';
-
-export const transformerVersions = {
-  'markmap-lib': 'process.env.VERSION',
-  d3: process.env.D3_VERSION,
-};
--- markmap-lib/src/plugins/index.ts
+++
@@ -1,8 +0,0 @@
-import katex from './katex';
-import prism from './prism';
-import frontmatter from './frontmatter';
-import npmUrl from './npm-url';
-import hljs from './hljs';
-
-export * from './base';
-export const plugins = [frontmatter, katex, hljs, npmUrl];
